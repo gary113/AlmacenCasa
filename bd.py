5b976c44ce3b34c89fa5fd1ea4db81a39089012a
Gary <gr.candia93@gmail.com>
2020-04-21 22:50:49 -0500

    Funcionalidad de búsqueda

--- bd.py
+++ bd.py
@@ -7,23 +7,16 @@ class BD():
 
         self.__conexion = connect('localhost', 'gary', 'chiky', 'inventario_casa')
 
-    def mostrar(self):
+    def buscarTodo(self):
 
         try:
 
             self.__conectar()
             cursor = self.__conexion.cursor()
+            sql='SELECT * FROM Producto'
 
-            cursor.execute('SELECT * FROM Producto')
+            cursor.execute(sql)
             resultados = cursor.fetchall()
-
-            for fila in resultados:
-                idProducto = fila[0]
-                descripcionProducto = fila[1]
-                cantidadProducto = fila[2]
-                unidadProducto = fila[3]
-                lugarProducto = fila[4]
-                #print(fila)
 
             self.__conexion.close()
 
@@ -34,4 +27,37 @@ class BD():
             print("Error {}: {}".format(e.args[0], e.args[1]))
 
             return None
+
+    def buscar(self,textoBuscar):
+        
+        if(textoBuscar==''):
+
+            return self.buscarTodo()
+
+        else:
+
+            try:
+
+                self.__conectar()
+                cursor = self.__conexion.cursor()
+
+                sql='''SELECT * FROM Producto WHERE
+                    id_producto LIKE "%{0}%" OR
+                    descripcion_producto LIKE "%{0}%" OR
+                    cantidad_producto LIKE "%{0}%" OR
+                    unidad_producto LIKE "%{0}%" OR
+                    lugar_producto LIKE "%{0}%"'''.format(textoBuscar)
+
+                cursor.execute(sql)
+                resultados = cursor.fetchall()
+
+                self.__conexion.close()
+
+                return resultados
+
+            except Error as e:
+
+                print("Error {}: {}".format(e.args[0], e.args[1]))
+
+                return None
 
--- principal.py
+++ principal.py
@@ -3,21 +3,62 @@ from tkinter import *
 from tkinter import ttk
 
 
+def buscar(tabla, listaTabla, textoBuscar, baseDatos):
+
+    limpiarTabla(tabla, listaTabla)
+    resultados = baseDatos.buscar(textoBuscar.get())
+
+    if(resultados != None):
+        for elemento in resultados:
+            listaTabla.append(tabla.insert('', END, text=elemento[0], values=elemento[1:]))
+
+
+def limpiarTabla(tabla, listaTabla):
+
+    if(len(listaTabla) > 0):
+        for elemento in listaTabla:
+            tabla.delete(elemento)
+
+        listaTabla.clear()
+
+
+def cancelar(tabla, listaTabla, textoBuscar, baseDatos):
+    textoBuscar.set('')
+    buscar(tabla, listaTabla, textoBuscar, baseDatos)
+
+
 baseDatos = BD()
-resultados=baseDatos.mostrar()
 
 ventana = Tk()
+ventana.title('Tabla de productos')
+ventana.geometry('+500+300')
 
-tabla=ttk.Treeview(ventana, columns=('Cantidad','Unidades','Lugar'))
-tabla.heading('#0', text='Descripción')
+ventana.grid_columnconfigure(1, weight=1)
+
+textoBuscar = StringVar()
+listaTabla = []
+
+lblBuscar = Label(ventana, text='Buscar:').grid(row=0, column=0)
+entryBuscar = Entry(ventana, textvariable=textoBuscar).grid(row=0, column=1, sticky=EW)
+btnBuscar = Button(ventana, text='Buscar', command=lambda: buscar(tabla, listaTabla, textoBuscar, baseDatos)).grid(row=0, column=2)
+btnCancelar = Button(ventana, text='Cancelar', command=lambda: cancelar(
+    tabla, listaTabla, textoBuscar, baseDatos)).grid(row=0, column=3, columnspan=2)
+
+tabla = ttk.Treeview(ventana, selectmode='browse', columns=('Descripción', 'Cantidad', 'Unidades', 'Lugar'))
+scrollTabla = ttk.Scrollbar(ventana, orient="vertical", command=tabla.yview)
+tabla.configure(yscrollcommand=scrollTabla.set)
+scrollTabla.grid(row=1, column=4, sticky=NS)
+tabla.heading('#0', text='ID')
+tabla.heading('Descripción', text='Descripción')
 tabla.heading('Cantidad', text='Cantidad')
 tabla.heading('Unidades', text='Unidades')
 tabla.heading('Lugar', text='Lugar')
 
-if(resultados!=None):
-    for elemento in resultados:
-        tabla.insert('', END, text=elemento[1], values=elemento[2:])
+buscar(tabla, listaTabla, textoBuscar, baseDatos)
 
-tabla.pack()
+tabla.grid(row=1, column=0, columnspan=4)
+
+# ventana2=Tk()
 
 ventana.mainloop()
+# ventana2.mainloop()
